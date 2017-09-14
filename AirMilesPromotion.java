f69d1494b7590e5406842f24247b04ae222dffcc
sansomlee <sansomlee@users.noreply.github.com>
2017-09-14 17:45:40 -0400

    Create AirMilesPromotion.java

---
+++ AirMilesPromotion.java
@@ -0,0 +1,17 @@
+class AirMilesPromption{
+
+        
+    /* Constructing the Airmiles promotion */
+    public AirMilesPromption(int budget, int[][] days){
+    }
+
+
+    /*
+    Find the maximum possible amount of miles 
+    that can be collected from the promotion.
+    */
+    public int findMaxMiles() {
+        return 0;
+    }
+    
+}
